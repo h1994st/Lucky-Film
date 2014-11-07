8e8726146a07336895cdcde67db5d261034b10d8
h1994st <h1994st@gmail.com>
2014-11-07 23:00:49 +0800

    simple layout

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/submit', function (req, res) {
+  console.log(req);
+  console.log(res);
 });
 
 module.exports = router;
